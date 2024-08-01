1fc962ba983ba010b424028905d08108779dbc56
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2024-08-01 17:42:02 +0300

    Add tests for App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div>
-        <h1>Hello world</h1>
-        <button>Click me</button>
-        <input type={"text"} placeholder={'input value...'}/>
-    </div>
-  );
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setData({});
+        }, 100)
+    }, [])
+
+    return (
+        <div>
+            {data && <div style={{color: 'red'}}>data</div>}
+            <h1>Hello world</h1>
+            <button>Click me</button>
+            <input type={"text"} placeholder={'input value...'}/>
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const helloWorldElem = screen.getByText(/hello world/i);
-  const button = screen.getByRole('button');
-  const input = screen.getByPlaceholderText(/input value/i);
-  expect(helloWorldElem).toBeInTheDocument();
-  expect(button).toBeInTheDocument();
-  expect(input).toMatchSnapshot();
-  screen.debug();
-});
+describe('TEST APP', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const helloWorldElem = screen.getByText(/hello world/i);
+    const button = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(/input value/i);
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+  });
+
+  test('test case', async () => {
+    render(<App/>);
+    const helloWorldElem = screen.queryByText(/hello2/i);
+    expect(helloWorldElem).toBeNull();
+    screen.debug();
+    const data = await screen.findByText(/data/i);
+    expect(data).toBeInTheDocument();
+    expect(data).toHaveStyle({color: 'red'})
+    screen.debug();
+  });
+})
